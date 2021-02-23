657a3ed46f9b5c1218686a436feafbe9b4024ee9
kalilzirak121 <kalilzirak@student.odisee.be>
2021-02-23 21:33:28 +0000

    While

---
+++ python/while-loop.py
@@ -0,0 +1,11 @@
+x=input("Enter a number to count to: ")
+x=int(x)
+y=1
+while True:
+    x=input("Enter a number to count to: ")
+    if x== 'q' or x == 'quit':
+while True:
+    print(y)
+    y=y+1
+    if y>x:
+        break
