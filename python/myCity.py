fc2df4e89eb7510a5e88548c4780f36282b3c60e
kalilzirak121 <kalilzirak@student.odisee.be>
2021-02-27 12:34:50 +0000

    City

---
+++ python/myCity.py
@@ -0,0 +1,7 @@
+def myCity(city):      #Define a function myCity with the argument city. 
+    print("I live in " + city + ".")        #print a statement that includes the city name.
+
+myCity("Austin")
+myCity("Tokyo")
+myCity("Salzburg")
+
